a95186439e923b7512f6005261e50774e887042a
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-03-31 03:24:21 +0200

    Improve Bin Loader

--- src/functions.h
+++ src/functions.h
@@ -225,8 +225,8 @@ void viewBaseline(uint8_t alternance)
   }
 }
 
-// List files on SPIFFS
-void getBinaryList(File dir)
+// List files on SPIFFS or SD
+void getBinaryList(File dir, String type)
 {
   while (true)
   {
@@ -238,14 +238,15 @@ void getBinaryList(File dir)
     }
 
     if (strstr(entry.name(), "/.") == NULL && strstr(entry.name(), ".bin") != NULL)
-    {
-      binFilename[binIndex] = entry.name();
+    { 
+      //Serial.println(type + "_" + entry.name());     
+      binFilename[binIndex] = type + "_" + entry.name();
       binIndex++;
     }
 
     if (entry.isDirectory() && strstr(entry.name(), "/.") == NULL)
     {
-      getBinaryList(entry);
+      getBinaryList(entry, type);
     }
 
     entry.close();
@@ -253,13 +254,17 @@ void getBinaryList(File dir)
 }
 
 // Bin Loader
-void binLoader() {
+void binLoader()
+{
   boolean click = 0;
   int8_t cursor = 0;
+  int8_t start = 0;
+  int8_t stop = 0;
+  int8_t limit = 8;
   int8_t change = 255;
   String tmpName;
 
-  if(!SPIFFS.begin())
+  if (!SPIFFS.begin())
   {
     Serial.println("SPIFFS Mount Failed");
 
@@ -276,36 +281,47 @@ void binLoader() {
 
     Serial.println("SPIFFS Formating...");
 
-    SPIFFS.format();    // Format SPIFFS...
+    SPIFFS.format(); // Format SPIFFS...
 
     M5.Lcd.setTextFont(0);
     M5.Lcd.setTextSize(0);
 
     return;
   }
-  
-  root = SPIFFS.open("/");
-  getBinaryList(root);
 
-  if(binIndex != 0) {
+  root = SPIFFS.open("/");
+  getBinaryList(root, "SP");
+
+
+  if (SD.begin()) {
+    root = SD.open("/");
+    getBinaryList(root, "SD");
+  }
+
+  if (binIndex != 0)
+  {
     M5.Lcd.setTextFont(1);
     M5.Lcd.setTextSize(1);
 
     M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
     M5.Lcd.setTextDatum(CC_DATUM);
 
-    for (uint8_t i = TIMEOUT_BIN_LOADER * 10; i > 0; i--) {
+    for (uint8_t i = TIMEOUT_BIN_LOADER * 10; i > 0; i--)
+    {
       getButton();
 
-      if( i % 10 == 0) {
+      if (i % 10 == 0)
+      {
         tmpName += ".";
         M5.Lcd.drawString(tmpName, 160, 20);
       }
 
-      if(btnA || btnC) {
+      if (btnA || btnC)
+      {
         return;
       }
-      else if(btnB) {
+      else if (btnB)
+      {
         click = 1;
         break;
       }
@@ -314,8 +330,10 @@ void binLoader() {
     }
   }
 
-  while(click == 1) {
-    while(btnB != 0) {
+  while (click == 1)
+  {
+    while (btnB != 0)
+    {
       getButton();
       vTaskDelay(100);
     }
@@ -325,36 +343,71 @@ void binLoader() {
 
     M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
     M5.Lcd.setTextDatum(CC_DATUM);
-    M5.Lcd.drawString("Bin Loader", 160, 20);
+    M5.Lcd.drawString("Bin Loader V0.2", 160, 20);
 
     getButton();
 
-    if(btnA) {
+    if (btnA)
+    {
       cursor--;
     }
-    else if(btnC) {
+    else if (btnC)
+    {
       cursor++;
     }
-    else if(btnB) {
-      updateFromFS(SPIFFS, binFilename[cursor]);
-      ESP.restart(); 
+    else if (btnB)
+    {
+      if(binFilename[cursor].substring(0, 4) == "SP_/") {
+        updateFromFS(SPIFFS, binFilename[cursor].substring(3));
+      }
+      else {
+        updateFromFS(SD, binFilename[cursor].substring(3));
+      }
+      ESP.restart();
     }
 
     cursor = (cursor < 0) ? binIndex - 1 : cursor;
     cursor = (cursor > binIndex - 1) ? 0 : cursor;
 
-    if(change != cursor) {
+    start = cursor / limit;
+
+    stop = (start * limit) + limit;
+    
+    /*
+    Serial.print(cursor);
+    Serial.print("-");
+    Serial.print(start);
+    Serial.print("-");
+    Serial.print(stop);
+    Serial.println("----------");
+    */
+
+    if (change != cursor)
+    {
       change = cursor;
       M5.Lcd.setTextPadding(320);
 
-      for(uint8_t i = 0; i < binIndex; i++) {
-        tmpName = binFilename[i].substring(1);
+      uint8_t i = 0;
+      for (uint8_t j = (start * limit); j < stop; j++)
+      {
+        tmpName = binFilename[j].substring(4);
 
-        if (cursor == i) {
+        if (cursor == j)
+        {
           tmpName = ">> " + tmpName + " <<";
+
+          M5.Lcd.setTextSize(1);
+          if(binFilename[cursor].substring(0, 4) == "SP_/") {
+            M5.Lcd.drawString("SPI Flash File Storage", 160, 50);
+          }
+          else {
+            M5.Lcd.drawString("SD Card Storage", 160, 50);
+          }
         }
-        
-        M5.Lcd.drawString(tmpName, 160, 60 + i * 20);
+
+        M5.Lcd.setTextSize(2);
+        M5.Lcd.drawString(tmpName, 160, 80 + i * 20);
+        i++;
       }
     }
     vTaskDelay(100);
